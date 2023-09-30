82314a7031f8071a56bb2c0964bb0708019ad4f1
master <sungwoony@kakao.com>
2023-09-30 23:15:20 +0900

    moment 라이브러리 추가, 컴포넌트 분리, 검색 날짜 지정(차주 일~토)

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ConcertList from '../src/app/components/ConceretList';
 import convert from 'xml-js';
 import axios from 'axios';
+import Footer from '../src/app/components/Footer';
+import moment from 'moment';
 
 const Index = ({ data }) => {
+  console.log(data);
+  
   return (
     <>
-      <ConcertList list={data != null ? data.dbs.db : []}/>
+      {/* <ConcertList list={data.dbs.db != null ? data.dbs.db : []}/>
+      <Footer /> */}
     </>
   );
 };
@@ -14,9 +19,15 @@ const Index = ({ data }) => {
 export default Index
 
 export async function getServerSideProps() {
-  const API_KEY = process.env.API_KEY;
+  const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+  const API_KEY2 = process.env.NEXT_PUBLIC_API_KEY2;
   
-  const res = await axios.get("http://www.kopis.or.kr/openApi/restful/pblprfr?service=" + API_KEY + "&stdate=20230924&eddate=20230930&cpage=1&rows=10&prfstate=01")
+  let from = moment().endOf('week').add(1,'d');
+  let to = moment().endOf('week').add(1,'d').endOf('week');
+
+
+  const res = await axios.get(`http://www.culture.go.kr/openapi/rest/publicperformancedisplays/period?from=${from}8&to=${to}&cPage=1&rows=10&place=&gpsxfrom=&gpsyfrom=&gpsxto=&gpsyto=&keyword=&sortStdr=1&serviceKey=${API_KEY2}`)
+   //const res = await axios.get(`http://www.kopis.or.kr/openApi/restful/pblprfr?service=${API_KEY}&stdate=${from}&eddate=${to}&cpage=1&rows=10&prfstate=01`)
   .then(response => {
     return convert.xml2json(response.data, { compact: true, spaces: 4 })
   });
@@ -26,4 +37,5 @@ export async function getServerSideProps() {
       data : JSON.parse(res)
     }
   }
+
 };
